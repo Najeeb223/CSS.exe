5a2c4029c1a6b739138ecb807eec6aae3d36f7c0
najeebabdi <najeebwarsame@gmail.com>
2025-05-15 20:29:35 +0200

    JS update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
-function scrollToLegendSectiom() {
-    
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollBtn = document.getElementById("start-btn");
+    const legendSection = document.querySelector(".legend");
+  
+    scrollBtn.addEventListener("click", () => {
+      legendSection.scrollIntoView({ behavior: "smooth" });
+    });
+  });
