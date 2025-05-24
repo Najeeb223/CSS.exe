b70ca66bf56ca967a8be4b1d336a9a0ec4771950
najeebabdi <najeebwarsame@gmail.com>
2025-05-24 20:40:41 +0200

    player selector update

--- script.js
+++ script.js
@@ -13,14 +13,38 @@ function scrollToLegendSection() {
 scrollToLegendSection();
 
 function selectPlayers() {
-
     const choosePlayersBtn = document.getElementById("grid-one-btn");
+    const selector = document.getElementById("player-selector");
 
-    console.log("Button:", choosePlayersBtn);
+    const playerOne = document.getElementById("player-one");
+    const playerTwo = document.getElementById("player-two");
+    const playerThree = document.getElementById("player-three");
+    const playerFour = document.getElementById("player-four");
+
+    const chamberGrid = document.getElementById("chamber-grid");
+
+    const chambers = [playerOne, playerTwo, playerThree, playerFour];
 
     choosePlayersBtn.addEventListener("click", function () {
-            console.log("SELECTOR BUTTON CLICKED");
+        let index = 0;
+
+        const intervalId = setInterval(() => {
+            const currentChamber = chambers[index];
+            const chamberRect = currentChamber.getBoundingClientRect();
+            const gridRect = chamberGrid.getBoundingClientRect();
+
+            const left = chamberRect.left - gridRect.left;
+            const top = chamberRect.top - gridRect.top;
+
+            selector.style.left = left + "px";
+            selector.style.top = top + "px";
+
+            index++;
+            if (index >= chambers.length) {
+                index = 0;
+            }
+        }, 500);
     });
-    
 }
+
 selectPlayers();
